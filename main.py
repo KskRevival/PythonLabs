da88be1f2cba42b353ee6375a7b63502a77cb035
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-05-25 20:40:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ import benchmark
 import plotbuilder
 import textgen
 
-
 if __name__ == '__main__':
     x = []  # Needle length
     standard = []
     rabin = []
     bauer = []
     kmp = []
-    
+    is_time = False
+
     for i in range(7, 20):
-        (hay, needle) = textgen.get_fixed_text(r.randint(2 ** (i - 7), 2 ** (i - 3)), 100000)
-        isTime = False
-        standard.append(benchmark.test(algos.standard_search, hay, needle, isTime))
-        rabin.append(benchmark.test(algos.rabin_karp, hay, needle, isTime))
-        bauer.append(benchmark.test(algos.bauer_moore, hay, needle, isTime))
-        kmp.append(benchmark.test(algos.kmp, hay, needle, isTime))
+        (hay, needle) = textgen.get_fixed_text(r.randint(2 ** (i - 7), 2 ** (i - 3)), 10000)
+        standard.append(benchmark.test(algos.standard_search, hay, needle, is_time))
+        rabin.append(benchmark.test(algos.rabin_karp, hay, needle, is_time))
+        bauer.append(benchmark.test(algos.bauer_moore, hay, needle, is_time))
+        kmp.append(benchmark.test(algos.kmp, hay, needle, is_time))
         x.append(i)
-        
-    plotbuilder.draw(x, standard, rabin, bauer, kmp)
+
+    plotbuilder.draw(is_time, x, standard, rabin, bauer, kmp)
