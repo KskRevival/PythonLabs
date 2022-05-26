4a617dbea9f8990d3032c9cdb5b9e4b55accb1f9
Ivan <java.enjoyer@gmail.com>
2022-05-26 22:14:35 +0500

    Update textgen.py

--- textgen.py
+++ textgen.py
@@ -3,7 +3,7 @@ import random as r
 
 
 def get_fixed_text(needle_size, position):
-    needle = 'a'.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(needle_size))
+    needle = 'a{0}'.format(''.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(needle_size)))
     hay = ''.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(position)) + needle
     return hay, needle
 
