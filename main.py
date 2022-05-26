3ecbbe7ad6582610ab00a972014a568045ba73e7
Ivan <java.enjoyer@gmail.com>
2022-05-26 22:19:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,17 @@ import textgen
 
 if __name__ == '__main__':
     x = []  # Needle length
-    standard = []
-    rabin = []
-    bauer = []
-    kmp = []
-    aho = []
-    is_time = True
+    is_time = False
+    runners = {'Standard': plotbuilder.PlotFunc('Standard', 'b*'),
+               'Rabin': plotbuilder.PlotFunc('Rabin', 'g^'),
+               'Bauer': plotbuilder.PlotFunc('Bauer', 'm<'),
+               'KMP': plotbuilder.PlotFunc("KMP", 'rs'),
+               'Aho': plotbuilder.PlotFunc('Aho', 'yP')}
 
     for i in range(7, 20):
-        (hay, needle) = textgen.get_fixed_text(r.randint(2 ** (i - 7), 2 ** (i - 3)), 100000)
-        standard.append(benchmark.test(algos.standard_search, hay, needle, is_time))
-        rabin.append(benchmark.test(algos.rabin_karp, hay, needle, is_time))
-        bauer.append(benchmark.test(algos.bauer_moore, hay, needle, is_time))
-        kmp.append(benchmark.test(algos.kmp, hay, needle, is_time))
-        aho.append(benchmark.test(algos.aho, hay, needle, is_time))
+        (hay, needle) = textgen.get_fixed_text(r.randint(2 ** (i - 7), 2 ** (i - 3)), 10000)
+        for (key, value) in runners.items():
+            value.res.append(benchmark.test(algos.algos[key], hay, needle, is_time))
         x.append(i)
 
-    plotbuilder.draw(is_time, x,
-                     [plotbuilder.PlotFunc("Standard", standard, 'b*'),
-                      plotbuilder.PlotFunc("Rabin", rabin, 'g^'),
-                      plotbuilder.PlotFunc("Bauer", bauer, 'm<'),
-                      plotbuilder.PlotFunc("KMP", kmp, 'rs'),
-                      plotbuilder.PlotFunc("Aho", aho, 'yP')])
+    plotbuilder.draw(is_time, x, runners.values())
