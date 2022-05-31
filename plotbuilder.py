0cd69705e1da8ae94f9342ac804a742f0ae394ec
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-05-31 21:55:44 +0500

    Update plotbuilder.py

--- plotbuilder.py
+++ plotbuilder.py
@@ -15,13 +15,15 @@ def draw(is_time, x, func_res):
     ax1 = plt.figure(figsize=(12, 7)).add_subplot(111)
 
     for func in func_res:
-        plt.plot(x, func.res, func.color, alpha=0.7, label=func.name, mew=2, ms=10)
+        plt.plot(x, func.res, func.color,
+                 alpha=0.7, label=func.name, mew=2, ms=10)
 
     plt.legend()
     if is_time:
         ax1.set_title(u'Time By Needle Length')
-    ax1.set_title(u'Memory By Needle Length')
-    plt.xlabel(u'Needle length [2^n]', fontsize=12)
-    plt.ylabel(u'Time [ms]', fontsize=12)
+        plt.ylabel(u'Time [ms]', fontsize=12)
+    else:
+        ax1.set_title(u'Memory By Needle Length')
+        plt.xlabel(u'Needle length [2^n]', fontsize=12)
     plt.grid(True)
     plt.show()
