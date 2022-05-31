715e0170a5e78eabc3e62d9f069bdc51d800664a
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-05-31 21:55:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import textgen
 
 if __name__ == '__main__':
     x = []  # Needle length
-    is_time = False
+    is_time = True
     runners = {'Standard': plotbuilder.PlotFunc('Standard', 'b*'),
                'Rabin': plotbuilder.PlotFunc('Rabin', 'g^'),
                'Bauer': plotbuilder.PlotFunc('Bauer', 'm<'),
@@ -14,9 +14,11 @@ if __name__ == '__main__':
                'Aho': plotbuilder.PlotFunc('Aho', 'yP')}
 
     for i in range(7, 20):
-        (hay, needle) = textgen.get_fixed_text(r.randint(2 ** (i - 7), 2 ** (i - 3)), 10000)
+        (hay, needle) = textgen.get_fixed_text(r.randint(2 ** (i - 7),
+                                                         2 ** (i - 3)), 10000)
         for (key, value) in runners.items():
-            value.res.append(benchmark.test(algos.algos[key], hay, needle, is_time))
+            value.res.append(benchmark.test(algos.algos[key],
+                                            hay, needle, is_time))
         x.append(i)
 
     plotbuilder.draw(is_time, x, runners.values())
