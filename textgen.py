f6e0b83901d7e5d8f8a748d9d431197389fafa92
senyapekus <92165711+senyapekus@users.noreply.github.com>
2022-05-31 21:55:58 +0500

    Update textgen.py

--- textgen.py
+++ textgen.py
@@ -2,13 +2,21 @@ import string
 import random as r
 
 
+ascii_uppercase = string.ascii_uppercase
+digits = string.digits
+
+
 def get_fixed_text(needle_size, position):
-    needle = 'a{0}'.format(''.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(needle_size)))
-    hay = ''.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(position)) + needle
+    needle = 'a{0}'.format(''.join(r.choice(ascii_uppercase + digits)
+                                   for _ in range(needle_size)))
+    hay = ''.join(r.choice(ascii_uppercase + digits)
+                  for _ in range(position)) + needle
     return hay, needle
 
 
 def get_random_text(needle_size, position):
-    needle = ''.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(needle_size))
-    hay = ''.join(r.choice(string.ascii_uppercase + string.digits) for _ in range(position))
+    needle = ''.join(r.choice(ascii_uppercase + digits)
+                     for _ in range(needle_size))
+    hay = ''.join(r.choice(ascii_uppercase + digits)
+                  for _ in range(position))
     return hay, needle
